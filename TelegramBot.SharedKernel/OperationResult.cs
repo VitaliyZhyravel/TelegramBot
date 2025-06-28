780319fcb73dbb8d991856a50e2ac56bfd1b424e
Zhuravel Vitaliy <vitaliizhuravel.it@gmail.com>
2025-06-28 14:11:12 +0300

    init

---
+++ TelegramBot.SharedKernel/OperationResult.cs
@@ -0,0 +1,13 @@
+﻿namespace TelegramBotConsole;
+
+public class OperationResult<TData>
+{
+    public bool IsSuccess { get; private set; }
+    public TData? Data { get; private set; }
+    public string? ErrorMesage { get; private set; }
+
+    public static OperationResult<TData> Sucssecc(TData data) => new OperationResult<TData>
+    { IsSuccess = true, Data = data };
+    public static  OperationResult<TData> Failure(string errorMessage) => new OperationResult<TData>
+    { IsSuccess = false, ErrorMesage = errorMessage };
+}
