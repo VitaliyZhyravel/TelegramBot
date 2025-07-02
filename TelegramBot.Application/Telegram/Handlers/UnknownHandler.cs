dc200945fb6f48713ac2d457413c0f3192dbcf89
Zhuravel Vitaliy <vitaliizhuravel.it@gmail.com>
2025-07-02 16:40:21 +0300

    add unknown message handler

---
+++ TelegramBot.Application/Telegram/Handlers/UnknownHandler.cs
@@ -0,0 +1,26 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using TelegramBot.Application.Interfaces.Handlers;
+
+namespace TelegramBot.Application.Telegram.Handlers
+{
+    public class UnknownHandler : IUnknownHandler
+    {
+        private readonly ITelegramBotClient _botClient;
+
+        public UnknownHandler(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+
+        public async Task UnknownMessageHandler(Update update)
+        {
+            var chatId = update.Message?.Chat.Id;
+
+            if (chatId != null)
+            {
+                await _botClient.SendMessage(chatId.Value, "Введений Вами текст, не зовсім зрозумілий, спробуйте \r\nскористатись кнопками, що виведені нижче. \r\n👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇");
+            }
+        }
+    }
+}
